59deacc13f513a127186f0faf3ec92b17080dda4
Anastasya <gavrilovaanastasya@yandex.ru>
2022-09-26 13:17:31 +0300

    принимает число и выдает таблицу квадратов чисел от 1 до n

---
+++ Seminar3.4/Program.cs
@@ -0,0 +1,11 @@
+﻿// Принимает число и выдает таблицу квадратов чисел от 1 до n
+
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+for (int i = 1; i<=number; i++)
+{
+    Math.Pow(i, 2);
+    Console.Write(i +" ");
+}
+
